33b8cfbc9d779851a6fa3d580b71a54a32c8e8ce
KamilOgonowski <kml.ogonowski@gmail.com>
2023-09-17 01:28:02 +0200

    4th commit - three rockets + space guns

--- src/Main.java
+++ src/Main.java
@@ -66,44 +66,45 @@ public class Main {
 
 
 
-//        for (int i=0; i<n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
-//                    System.out.print("*");
-//                else
-//                    System.out.print(" ");
-//            }
-//
-//            for (int j = 0; j < n; j++) {
-//                if (i == 0 || i == n - 1 || j == n - 1)
-//                    System.out.print("*");
-//                else
-//                    System.out.print(" ");
-//            }
-//            for (int j = 0; j < n; j++) {
-//                if (i == 0 || i == n - 1 || j == n - 1)
-//                    System.out.print("*");
-//                else
-//                    System.out.print(" ");
-//            }
-//
-//            for (int j = 0; j < n; j++) {
-//                if (i == 0 || i == n - 1 || j == n - 1) {
-//                    System.out.print("*");
-//                }
-//                else {
-//                    System.out.print(" ");
-//                }
-//            }
-//
-//            for (int j = 0; j < n; j++) {
-//                if (i == 0 || i == n - 1 || j == n - 1)
-//                    System.out.print("*");
-//                else
-//                    System.out.print(" ");
-//            }
-//            System.out.println();
-//        }
+        for (int i=0; i<n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1 && i<(n-1)/2 || i>3*(n-1)/4 && j==n-1 )
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (i==0 && j==(n-1)/2  || i==(n-1)/2 || i+j >= (n-1)/2 && j-i<=(n-1)/2 && i<=(n-1)/2 || i==3*(n-1)/4)
+                    System.out.print("o");
+                else
+                    System.out.print(" ");
+            }
+            for (int j = 0; j < n; j++) {
+//                if (i == 0 || i == n - 1 || j==0 && i!= 3*(n-1)/4 || j == n - 1 && i!= 3*(n-1)/4 )
+                if (i == 0 || i == n - 1 || j==0 && i<(n-1)/2 || i>3*(n-1)/4 && j==0 || j == n - 1 && i<(n-1)/2 || i>3*(n-1)/4 && j==n-1  )
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (i==0 && j==(n-1)/2  || i==(n-1)/2 || i+j >= (n-1)/2 && j-i<=(n-1)/2 && i<=(n-1)/2 || i==3*(n-1)/4)
+                    System.out.print("o");
+
+                else {
+                    System.out.print(" ");
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == n - 1 ||  j==0 && i<(n-1)/2 || j==0 && i>3*(n-1)/4)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
 //
 //
 //
