52b36ddc33157b3ea63be36f0e71ed8ff201d9b6
KamilOgonowski <kml.ogonowski@gmail.com>
2023-09-17 00:39:24 +0200

    third commit - three rockets (upper part)

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
                 System.out.print(" ");
             }
             for (int j = 0; j < n; j++) {
-                if (i+j==(n-1)/2 ||  j-i==(n-1)/2 || i==(n-1)/2 || j==0 && i>=(n-1)/2 || j==n-1 && i>=(n-1)/2 || i==3*(n-1)/4)
+                if (i+j==(n-1)/2 ||  j-i==(n-1)/2 || i>=(n-1)/2 && i<=3*(n-1)/4 || j==0 && i>=(n-1)/2 || j==n-1 && i>=(n-1)/2)
                     System.out.print("*");
                 else
                     System.out.print(" ");
@@ -35,36 +35,28 @@ public class Main {
 
         for (int i=0; i<n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+                if (i+j==(n-1)/2 ||  j-i==(n-1)/2 || i==(n-1)/2 || j==0 && i>=(n-1)/2 || j==n-1 && i>=(n-1)/2)
                     System.out.print("*");
                 else
                     System.out.print(" ");
             }
 
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1 || j == n - 1)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
+                System.out.print(" ");
             }
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1 || j == n - 1)
+                if (j==0 || j==n-1 || i>(n-1)/2 && i<= 3*(n-1)/4 )
                     System.out.print("*");
                 else
                     System.out.print(" ");
             }
 
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1 || j == n - 1) {
-                    System.out.print("*");
-                }
-                else {
-                    System.out.print(" ");
-                }
+                System.out.print(" ");
             }
 
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1 || j == n - 1)
+                if (i+j==(n-1)/2 ||  j-i==(n-1)/2 || i==(n-1)/2 || j==0 && i>=(n-1)/2 || j==n-1 && i>=(n-1)/2)
                     System.out.print("*");
                 else
                     System.out.print(" ");
